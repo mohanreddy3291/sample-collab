27e2c6ccbe434c9f4a3b8af4b13802489b914304
Mohan reddy <mohan4gp@gmail.com>
2023-02-03 14:48:30 -0500

    sushanth

---
+++ src/main/java/com/example/springboot/Sample.java
@@ -0,0 +1,4 @@
+package com.example.springboot;
+
+public class Sample {
+}
---
+++ src/main/resources/test.java
@@ -0,0 +1,8 @@
+class test {
+    int a =20;
+            int b = 56;
+            int c = a+b;
+    public static void main(String[] args) {
+        System.out.println(c);
+    }
+}
