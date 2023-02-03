45ae07de429cd8dc964d9aa54582b48835f053cd
Mohan reddy <mohan4gp@gmail.com>
2023-02-03 12:17:44 -0500

    initial commit

---
+++ src/main/java/com/example/springboot/HelloController.java
@@ -0,0 +1,22 @@
+package com.example.springboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+
+
+	@GetMapping("/saveData")
+	public  String saveData() {
+
+
+		return "";
+	}
+
+}
