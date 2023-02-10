76df7a507f5be30492c9588af7f397f986bf3f47
Sowmyamanda26 <124391245+Sowmyamanda26@users.noreply.github.com>
2023-02-10 17:43:28 -0600

    main

--- src/main/resources/main.kt
+++ src/main/resources/main.kt
@@ -1,2 +1,14 @@
-class main {
+class Main {
+    var x = 5
+
+    companion object {
+        @kotlin.jvm.JvmStatic
+        fun main(args: Array<String>) {
+            val myObj1 = Main() // Object 1
+            val myObj2 = Main() // Object 2
+            println(myObj1.x)
+            println(myObj2.x)
+        }
+    }
 }
+
