48e3778e233dca4e19d396125146fd19367cbd01
sushanth4646 <teegalasushanth@gmail.com>
2023-02-10 17:53:17 -0600

    sushanth file commit

--- src/main/java/com/example/springboot/Sushanth.java
+++ src/main/java/com/example/springboot/Sushanth.java
@@ -2,5 +2,9 @@ package com.example.springboot;
 
 public class Sushanth {
 
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+
     // Hello this is sushanth
 }
