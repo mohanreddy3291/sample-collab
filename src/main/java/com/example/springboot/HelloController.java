0700be7cee32965ae4de5456602e7d125f9ae1f5
peddollamanu <peddollamanu1415@gmail.com>
2023-02-20 18:04:31 -0500

    code for multiply

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -1,6 +1,8 @@
 package com.example.springboot;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,4 +21,17 @@ public class HelloController {
 		return "";
 	}
 
+	@GetMapping("api/multiply/{num}")
+	public Integer multiply(@PathVariable("num") Integer num, @RequestParam("number") Integer number){
+		Integer mul=num*number;
+		return mul;
+	}
+	/*@GetMapping("/api/multiply/{num1}/{num2}")
+	public Integer mul(@PathVariable ("num1") Integer num1, @PathVariable ("num2") Integer num2){
+		Integer mul=num1*num2;
+		return mul;
+	}*/
+
 }
+//public Integer sum(@PathVariable("number1") Integer number1, @PathVariable("number2") Integer number2) {
+//localhost:8082/api/multiply/2?number=120
