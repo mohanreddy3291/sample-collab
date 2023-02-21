dc85fe851e26e099c57c713c27e625395f09340f
Kundana <kundanareddy.791@gmail.com>
2023-02-20 21:33:03 -0600

    Rest Api- multiply task added

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -1,22 +1,35 @@
 package com.example.springboot;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+@GetMapping("/api/sum")
+public Integer sum(@RequestParam("number1") Integer number1,
+				   @RequestParam("number2") Integer number2)
+{
+	Integer sum = number1 + number2;
+	return sum;
+}
+@GetMapping("/api/total/{first}/{second}")
+public Integer total(@PathVariable("first") Integer first,
+				   @PathVariable("second") Integer second)
+{
+	Integer total = first+second;
+	return total;
+}
 
-	@GetMapping("/")
-	public String index() {
-		return "Greetings from Spring Boot!";
-	}
+@GetMapping("/api/multiply")
+	public Integer multiply(@RequestParam("number1") Integer number1,
+							@RequestParam("number2") Integer number2)
+{
+	Integer multiply = number1 * number2;
+	return multiply;
+}
 
 
-	@GetMapping("/saveData")
-	public  String saveData() {
-
-
-		return "";
-	}
 
 }
