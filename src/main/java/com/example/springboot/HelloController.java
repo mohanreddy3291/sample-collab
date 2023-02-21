f901d80715e51c0eab8465ad2fed26ff08ad3fbf
Mohan reddy <mohan4gp@gmail.com>
2023-02-21 16:15:43 -0500

    Requuest Param

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -1,5 +1,8 @@
 package com.example.springboot;
 
+import com.example.springboot.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -7,6 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+	@Autowired
+	CustomerService customerService;
 
 	@GetMapping("/")
 	public String index() {
@@ -23,23 +29,19 @@ public class HelloController {
 
 	@GetMapping("/api/sum/{first}/{second}")
 	public Integer sum(@PathVariable("first") Integer first, @PathVariable("second") Integer second) {
-
-		Integer sum = first + second;
-		return sum;
-
-
+		return customerService.sum(first, second);
 	}
 
 
 	@GetMapping("/api/sum")
 	public Integer sumWithRequest(@RequestParam("number1") Integer number1, @RequestParam("number2") Integer number2) {
-
-		Integer sum = number1+  number2;
-
-		return sum;
+		return customerService.sum(number1, number2);
 	}
 
 	// localhost:8082/api/multiply/2?number=120  =expectedResult = 240 - use only reuest Param
+
+
+
 
 
 }
