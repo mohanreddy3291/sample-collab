c3c8a2fabc0ad9e1f4346041c943e424454b5e6f
kartheekk <kartheekkosaraju97@gmail.com>
2023-02-21 15:51:34 -0600

    Commit these files

--- src/main/java/com/example/springboot/Sample.java
+++ src/main/java/com/example/springboot/Sample.java
@@ -1,4 +1,14 @@
 package com.example.springboot;
 
 public class Sample {
+
+    //this is the Sample program
+
+    public static void main(String[] args) {
+
+        System.out.println("This is the sample code for Java file");
+    }
+
+
+
 }
