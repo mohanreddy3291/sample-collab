42ca2def8d6c2507182e7b3fb2d4488225a5e731
Mohan reddy <mohan4gp@gmail.com>
2023-02-24 14:48:47 -0600

    Requuest Param
    (cherry picked from commit 76adac00664202df9187ed363e1edbbe2590ed93)

--- src/main/java/com/example/springboot/controller/HelloController.java
+++ src/main/java/com/example/springboot/controller/HelloController.java
@@ -1,45 +1,90 @@
 package com.example.springboot.controller;
 
+import com.example.springboot.service.CustomerService;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @RestController
 public class HelloController {
-    @GetMapping("/api/sum")
-    public Integer sum(@RequestParam("number1") Integer number1,
-                       @RequestParam("number2") Integer number2)
-    {
-        Integer sum = number1 + number2;
-        return sum;
-    }
-    @GetMapping("/api/total/{first}/{second}")
-    public Integer total(@PathVariable("first") Integer first,
-                         @PathVariable("second") Integer second)
-    {
-        Integer total = first+second;
-        return total;
-    }
 
-    @GetMapping("/api/multiply")
-    public Integer multiply(@RequestParam("number1") Integer number1,
-                            @RequestParam("number2") Integer number2) {
-        Integer multiply = number1 * number2;
-        return multiply;
-    }
-    @PostMapping("/api/collections")
-    public void postData(@RequestBody List<Integer> inputData)
-    {
-        for(Integer test:inputData)
-        {
-            System.out.println(test*2);
-        }
-    }
+	@Autowired
+	CustomerService customerService;
 
-    @PostMapping("/api/postBody")
-    public Boolean postBody(@RequestBody String input, @RequestParam("no") Boolean no)
-    {
-        System.out.println(input);
-        return no;
-    }
+	@GetMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+
+
+	@GetMapping("/saveData")
+	public  String saveData() {
+
+
+		return "";
+	}
+
+	@GetMapping("/api/sum/{first}/{second}")
+	public Integer sum(@PathVariable("first") Integer first, @PathVariable("second") Integer second) {
+		return customerService.sum(first, second);
+	}
+
+
+	@GetMapping("/api/sum")
+	public Integer sumWithRequest(@RequestParam("number1") Integer number1, @RequestParam("number2") Integer number2) {
+		return customerService.sum(number1, number2);
+	}
+
+	// localhost:8082/api/multiply/2?number=120  =expectedResult = 240 - use only reuest Param
+
+
+	@PostMapping("/api/collection")
+	public void postData(@RequestBody List<Integer> inputData) {
+		//this is test committed file
+
+		List<Integer> testList = new ArrayList<>();
+		testList.add(1);
+		testList.add(2);
+		testList.add(3);
+		testList.add(4);
+		testList.add(5);
+		testList.add(3);
+		testList.add(4);
+		for (Integer test: testList) {
+			System.out.println(test);
+		}
+
+		Set<Integer> testSet = new HashSet<>();
+		testSet.add(1);
+		testSet.add(1);
+		testSet.add(3);
+		testSet.add(54);
+		testSet.add(2);
+		testSet.add(3);
+		testSet.add(2);
+
+		for (Integer testSEtt: testSet) {
+			System.out.println(testSEtt);
+		}
+
+//		1. // Input ["Name -", "Class -", "Subject- "] - output - ["Name - xxcxcfdg", "Class - fdfg", "Subject- fsdg"]
+		//2. Input [2, 4, 6, 8, 16] - output - [4, 8, 12, 16,32]
+		// 3. Input [1,2,3,45,5,5,5,7,7,88] - output - 	Input [1,2,3,45,5,7,88]
+
+
+
+
+
+
+	}
+
+
+
+
 }
