76adac00664202df9187ed363e1edbbe2590ed93
Mohan reddy <mohan4gp@gmail.com>
2023-02-24 15:48:47 -0500

    Requuest Param

--- src/main/java/com/example/springboot/controller/HelloController.java
+++ src/main/java/com/example/springboot/controller/HelloController.java
@@ -1,11 +1,15 @@
 package com.example.springboot.controller;
 
 import com.example.springboot.service.CustomerService;
+import jakarta.persistence.criteria.CriteriaBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @RestController
 public class HelloController {
@@ -43,10 +47,39 @@ public class HelloController {
 	@PostMapping("/api/collection")
 	public void postData(@RequestBody List<Integer> inputData) {
 		//this is test committed file
-		for (Integer test: inputData) {
-			System.out.println(test);
 
+		List<Integer> testList = new ArrayList<>();
+		testList.add(1);
+		testList.add(2);
+		testList.add(3);
+		testList.add(4);
+		testList.add(5);
+		testList.add(3);
+		testList.add(4);
+		for (Integer test: testList) {
+			System.out.println(test);
 		}
+
+		Set<Integer> testSet = new HashSet<>();
+		testSet.add(1);
+		testSet.add(1);
+		testSet.add(3);
+		testSet.add(54);
+		testSet.add(2);
+		testSet.add(3);
+		testSet.add(2);
+
+		for (Integer testSEtt: testSet) {
+			System.out.println(testSEtt);
+		}
+
+//		1. // Input ["Name -", "Class -", "Subject- "] - output - ["Name - xxcxcfdg", "Class - fdfg", "Subject- fsdg"]
+		//2. Input [2, 4, 6, 8, 16] - output - [4, 8, 12, 16,32]
+		// 3. Input [1,2,3,45,5,5,5,7,7,88] - output - 	Input [1,2,3,45,5,7,88]
+
+
+
+
 
 
 	}
