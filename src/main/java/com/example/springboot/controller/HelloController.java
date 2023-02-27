2feef551971ed7d3f3813ab4da9da5b4cf533153
Mohan reddy <mohan4gp@gmail.com>
2023-02-27 16:01:54 -0500

    Requuest Param

--- src/main/java/com/example/springboot/controller/HelloController.java
+++ src/main/java/com/example/springboot/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.example.springboot.controller;
 
 import com.example.springboot.service.CustomerService;
 import jakarta.persistence.criteria.CriteriaBuilder;
+import org.hibernate.persister.entity.SingleTableEntityPersister;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.web.bind.annotation.*;
@@ -72,10 +73,44 @@ public class HelloController {
 		for (Integer testSEtt: testSet) {
 			System.out.println(testSEtt);
 		}
+	}
 
-//		1. // Input ["Name -", "Class -", "Subject- "] - output - ["Name - xxcxcfdg", "Class - fdfg", "Subject- fsdg"]
-		//2. Input [2, 4, 6, 8, 16] - output - [4, 8, 12, 16,32]
-		// 3. Input [1,2,3,45,5,5,5,7,7,88] - output - 	Input [1,2,3,45,5,7,88]
+
+
+		@PostMapping("/api/testTask")
+		public List<String> testTask(@RequestBody List<String> inputData) {
+				List<String> listToReturn = new ArrayList<>();
+		for (String test: inputData) {
+			if (test.equalsIgnoreCase("Name -")) {
+				String data = test + "-testName";
+				listToReturn.add(data);
+			}
+			if (test.equalsIgnoreCase("Class -")) {
+				String data = test + "-testClass";
+				listToReturn.add(data);
+			}
+			if (test.equalsIgnoreCase("Subject -")) {
+				String data = test + "-testSubject";
+				listToReturn.add(data);
+			}
+
+		}
+		return listToReturn;
+	}
+
+
+	@PostMapping("/api/setExample")
+	public List<Integer> setExample(@RequestBody List<Integer> inputData) {
+		List<Integer> returnList = new ArrayList<>();
+		Set<Integer> set = new HashSet<>();
+		for (Integer input : inputData) {
+			set.add(input);
+		}
+		returnList.addAll(set);
+		return  returnList;
+	}
+
+
 
 
 
@@ -84,7 +119,6 @@ public class HelloController {
 
 	}
 
-
-
-
-}
+//		1. // Input ["Name -", "Class -", "Subject- "] - output - ["Name - testName", "Class - testClass", "Subject- testSubject"]
+//2. Input [2, 4, 6, 8, 16] - output - [4, 8, 12, 16,32]
+// 3. Input [1,2,3,45,5,5,5,7,7,88] - output - 	Input [1,2,3,45,5,7,88]
