69e120802c795e7b2e441eb9d868922005511e43
Rajureddy7675 <rajureddymamidi7675@gmail.com>
2023-02-28 15:11:32 -0600

    commit12

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -11,21 +11,22 @@ import java.util.List;
 @RestController
 public class HelloController {
 
-	@GetMapping("/")
-	public String index() {
-		return "Greetings from Spring Boot!";
-	}
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
 
 
-	@GetMapping("/saveData")
-	public  String saveData() {
+    @GetMapping("/saveData")
+    public String saveData() {
 
 
-		return "";
-	}
+        return "";
+    }
+
     @PostMapping("/laptop")
-    public void a(@RequestBody List<String> input){
-        for(String b:input){
+    public void a(@RequestBody List<String> input) {
+        for (String b : input) {
             System.out.println(b);
 
         }
@@ -34,15 +35,37 @@ public class HelloController {
         else
             System.out.println("False");
     }
+
     @PostMapping("/Bag")
-    public List<Integer> c(@RequestBody List<Integer> input1){
+    public List<Integer> c(@RequestBody List<Integer> input1) {
         List<Integer> e = new ArrayList<>();
-        for(Integer d : input1){
-            e.add((d/2)+2);
+        for (Integer d : input1) {
+            e.add((d/2) + 2);
         }
         return e;
     }
 
+    @PostMapping("/testTask")
+    public List<String> testTask(@RequestBody List<String> inputData) {
+        List<String> listToReturn = new ArrayList<>();
+        for (String test : inputData) {
+            if (test.equalsIgnoreCase("Name -")) {
+                String data =  "-Name01";
+                listToReturn.add(data);
+            }
+            if (test.equalsIgnoreCase("Class -")) {
+                String data =  "-Class02";
+                listToReturn.add(data);
+            }
+            if (test.equalsIgnoreCase("Subject -")) {
+                String data =  "-Subject03";
+                listToReturn.add(data);
+            }
 
+        }
+
+
+        return listToReturn;
+    }
 }
 
