930491caead979deb818084b4baf57ee7093c053
sumanbazar123 <124317019+sumanbazar123@users.noreply.github.com>
2023-02-28 16:24:09 -0600

    tasks

--- src/main/java/com/example/springboot/controller/HelloController.java
+++ src/main/java/com/example/springboot/controller/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.util.*;
 
 @RestController
 public class HelloController {
@@ -20,7 +20,7 @@ public class HelloController {
 
 
 	@GetMapping("/saveData")
-	public  String saveData() {
+	public String saveData() {
 
 
 		return "";
@@ -43,15 +43,97 @@ public class HelloController {
 	@PostMapping("/api/collection")
 	public void postData(@RequestBody List<Integer> inputData) {
 		//this is test committed file
-		for (Integer test: inputData) {
+		for (Integer test : inputData) {
 			System.out.println(test);
+
+		}
+	}
+
+	@PostMapping("api/task")
+	public void postData(@RequestBody Stack<String> inputData) {
+
+		for (String i : inputData) {
+			System.out.println(i.toCharArray());
+			//System.out.println(i.isEmpty());
+			//ArrayList<String> ar=new ArrayList<>(inputData);
+		}
+		if (inputData.isEmpty())
+			System.out.println("True");
+		else
+			System.out.println("False");
+	}
+		/*if (inputData.isEmpty())
+			System.out.println("True");
+		 else
+			System.out.println("False");
+
+		 */
+
+		/*System.out.println("The List Before Remove operation:");
+		System.out.println(inputData);
+		System.out.println(inputData.remove(0));
+		System.out.println("The list after remove operation:");
+		System.out.println(inputData);*/
+@PostMapping("api/task2")
+public void postData(@RequestBody Vector<Integer> inputData) {
+
+	String a, b;
+	for (int i = 0; i < inputData.size(); i++) {
+		int s = inputData.get(i);
+		System.out.println("The list Before operation:" + s);
+		Integer result = s / 2 + 2;
+		System.out.println("The List After Operation:" + result.toString());
+		System.out.println(result.toString());
+	}
+
+}
+	@GetMapping("api/Mapping")
+	public void TestMap() {
+		Map<Integer, List<String>> map1 = new HashMap<>();
+		List<String> l = new Vector<>();
+		l.add("suman");
+		l.add("vineeth");
+		l.add("sai");
+		l.add("raj");
+		l.add("nagesh");
+		map1.put(1,l);
+		System.out.println(map1.get(1));
+	}
+	@GetMapping("/task/Highest")
+			public void high() {
+		List<Integer> ll = new Vector<>();
+		List<Integer> lll = new Vector<>();
+		ll.add(7);
+		ll.add(4);
+		ll.add(1);
+		ll.add(5);
+		ll.add(9);
+
+		lll.add(3);
+		lll.add(5);
+		lll.add(6);
+		lll.add(1);
+		lll.add(99);
+		//System.out.println(m.get(1));
+		ll.addAll(lll);
+		Integer max = Collections.max(ll);
+		System.out.println("Highest Element is:" + max);
+		System.out.println();
+	}
+	//another way of finding the highest value using single list
+	@GetMapping("task1/high")
+	public void big()
+		{
+			List<Integer> list= Arrays.asList(1,5,8,99,100);
+			Integer max=Collections.max(list);
+			System.out.println(max);
+		}
+
+
+
 
 		}
 
 
-	}
 
 
-
-
-}
