f528f0bef85a0d9d5a57c1001cee570bf07d8b37
Neethi-Reddy <Neethi-Reddy@users.noreply.github.com>
2023-02-27 18:05:24 -0600

    Task

--- src/main/java/com/example/springboot/controller/MyCollections.java
+++ src/main/java/com/example/springboot/controller/MyCollections.java
@@ -20,6 +20,42 @@ public class MyCollections {
         }
 
     }
-}
 
+    @PostMapping("/api/new")
+    public List<String> myTask(@RequestBody List<String> inputData) {
+        List<String> op = new ArrayList<>();
+        for (String test: inputData) {
+            if (test.equalsIgnoreCase("Check")) {
+                String data = test + "01";
+                op.add(data);
+            }
+            if (test.equalsIgnoreCase("Amount")) {
+                String data = test + "02";
+                op.add(data);
+            }
+            if (test.equalsIgnoreCase("Data")) {
+                String data = test + "03";
+                op.add(data);
+            }
+            if (test.equalsIgnoreCase("Deposit")) {
+                String data = test + "04";
+                op.add(data);
+            }
+
+        }
+        return op;
+    }
+
+
+    @PostMapping("/api/new2")
+    public List<Integer> myTask1(@RequestBody List<Integer> inputData) {
+        List<Integer> op = new ArrayList<>();
+        for (Integer test: inputData) {
+            Integer s = test/2 +2;
+            op.add(s);
+
+        }
+        return op;
+    }
+}
 
