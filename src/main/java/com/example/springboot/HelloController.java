30c1d411fcf135a8cea5b89f40c976b4acab3189
Akhilesh <akhileshsoma438@gmail.com>
2023-02-28 17:23:13 -0600

    first commit

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -1,7 +1,13 @@
 package com.example.springboot;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class HelloController {
@@ -13,10 +19,21 @@ public class HelloController {
 
 
 	@GetMapping("/saveData")
-	public  String saveData() {
+	public String saveData() {
 
 
 		return "";
 	}
+//input -
+	@PostMapping("/api/Bool")
+	public void bool(@RequestBody List<String> inputData) {
+		for( String a : inputData) {
+			if (a.isEmpty()) {
+				System.out.println("true");
+			} else {
+				System.out.println("false");
+			}
+		}
 
+	}
 }
