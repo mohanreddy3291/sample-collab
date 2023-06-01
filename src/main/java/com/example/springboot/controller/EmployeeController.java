fb170026c0909bcf44127389fbd8922b3cb1f633
sumanbazar123 <124317019+sumanbazar123@users.noreply.github.com>
2023-06-01 15:48:09 -0500

    Performed Update,Fetch/read operations

--- src/main/java/com/example/springboot/controller/EmployeeController.java
+++ src/main/java/com/example/springboot/controller/EmployeeController.java
@@ -1,11 +1,11 @@
 package com.example.springboot.controller;
 
 import com.example.springboot.entity.Employee;
+import com.example.springboot.repo.EmployeeRepository;
+import com.example.springboot.service.CrudOperation;
 import com.example.springboot.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,6 +14,10 @@ public class EmployeeController
 {
     @Autowired
     private EmployeeService employeeService;
+    @Autowired
+    private EmployeeRepository employeeRepository;
+    @Autowired
+    private CrudOperation crudOperation;
 
     @PostMapping("/api/EmployeeDetails")
     public List<Employee> saveEmployeeDetails(@RequestBody List<Employee> employeeList)
@@ -22,6 +26,20 @@ public class EmployeeController
                if(hasaddress && EmployeeService.hasvalidNames(employeeList) && EmployeeService.age(employeeList));
             return employeeService.saveemployee(employeeList);
     }
+    @GetMapping("/api/fetchempdetails")
+    public List<Employee> getemp(@RequestBody List<Employee> employeeList)
+    {
+        return employeeService.getemp(employeeList);
+    }
 
-
+    @PutMapping("/api/Updatedata/{emp_id}")
+    public Employee updateemp(@RequestBody Employee employee,@PathVariable int emp_id)
+    {
+        return employeeService.Update(employee,emp_id);
+    }
+@GetMapping("/api/getdetailsbyid/{emp_id}")
+    public Employee getdata(@PathVariable int emp_id)
+{
+    return employeeService.getempbyId(emp_id);
+}
 }
